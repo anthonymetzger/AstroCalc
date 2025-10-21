75437531cdc204674875afbcfb9c89c8f3f65484
Anthony Metzger <tony.m.metzger@gmail.com>
2025-10-21 17:25:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let planets = 0;
 let result = 0;
 
 window.onload = function () {
-  document.getElementById("PlutoNo").checked = true;
   isPlutoPlanet();
 }
 function isPlutoPlanet() {
@@ -22,37 +21,48 @@ function isPlutoPlanet() {
     return null;
   }
 }
-
-document.getElementById("plutoYes").addEventListener("click", function () {
-  document.getElementById("plutoPlanet").disabled = false;
-});
-document.getElementById("plutoNo").addEventListener("click", function () {
-  document.getElementById("plutoPlanet").disabled = true;
-  document.getElementById("planets").value = "null";
-});
+function checkOption() {
+  if (document.getElementById("planets").value == "null") {
+    document.getElementById("result").innerHTML = "You need to select a planet!";
+    return null;
+  }
+  else {
+    return true;
+  }
   
-  function callMath(w, p) {
-    if (isPlutoPlanet() == true) {
-      document.getElementById("plutoPlanet").disabled = false;
-      weight = w;
-      planets = p;
-      result = weight * planets;
-      document.getElementById("result").innerHTML = "Your weight on that planet is: " + result;
-      return result;
-    }
-    else if (isPlutoPlanet() == false) {
-      document.getElementById("plutoPlanet").disabled = true;
-      weight = w;
-      planets = p;
-      result = weight * planets;
-      document.getElementById("result").innerHTML = "Your weight on that planet is: " + result;
-      return result;
-    }
-    else if (isPlutoPlanet() == null) {
-      return 0;
-    }
-    
-    
+}
+  document.getElementById("plutoYes").addEventListener("click", function () {
+    document.getElementById("plutoPlanet").disabled = false;
+    document.getElementById("planets").value = "null";
+  });
+  document.getElementById("plutoNo").addEventListener("click", function () {
+    document.getElementById("plutoPlanet").disabled = true;
+    document.getElementById("planets").value = "null";
+  });
+
+
+function callMath(w, p) {
+  check = checkOption();
+  if (isPlutoPlanet() == true && check != null) {
+    document.getElementById("plutoPlanet").disabled = false;
+    weight = w;
+    planets = p;
+    result = weight * planets;
+    document.getElementById("result").innerHTML = "Your weight on that planet is: " + result + " lbs.";
+    return result;
+  }
+  else if (isPlutoPlanet() == false && check != null) {
+    document.getElementById("plutoPlanet").disabled = true;
+    weight = w;
+    planets = p;
+    result = weight * planets;
+    document.getElementById("result").innerHTML = "Your weight on that planet is: " + result + " lbs.";
+    return result;
+  }
+
+  else if (isPlutoPlanet() == null) {
+    return 0;
 
   }
+}
 
